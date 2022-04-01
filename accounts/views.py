c0e61b7d67f872485f1a0124c06c20e47a432e6f
kosei <harumi.kosei1106@gmail.com>
2022-04-01 22:35:15 +0900

    blog

---
+++ accounts/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render,redirect
+from allauth.account import views
+# Create your views here.
+
+class LoginView(views.LoginView):
+    template_name = 'accounts/login.html'
+
+class LogoutView(views.LogoutView):
+    template_name = 'accounts/logout.html'
+
+    def post(self,*args,**kwargs):
+        if self.request.user.is_authenticated:
+            self.logout()
+        return redirect('/')
+
+class SignupView(views.SignupView):
+    template_name = 'accounts/signup.html'
+    def post(self,*args,**kwargs):
+        if self.request.user.is_authenticated:
+            self.signup()
+        return redirect('/')
